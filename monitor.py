8b3e19d98daf583f6b4145fb5afc8015e23dd31f
Paal <paalriis@outlook.com>
2026-09-10 10:43:12 +0200

    Added disk usage metrics

--- monitor.py
+++ monitor.py
@@ -30,6 +30,17 @@ def cpu_usage():
     cpu_utilization = psutil.cpu_percent()
     return cpu_utilization
 
+def disk_usage():
+    """Calculates disk usage and returns percent, free and total_and_usage string in GB"""
+    total, used, free, percent = psutil.disk_usage("/")
+    total_in_gb = total / (1024 ** 3)
+    used_in_gb = used / (1024 ** 3)
+    free_in_gb = free / (1024 ** 3)
+    total_and_usage = f"{used_in_gb:.2f}GB / {total_in_gb:.2f}GB"
+
+    return percent, free_in_gb, total_and_usage
+
+
 def update_label(label, function, index=None):
     """Updates the label each second, takes label name and function as args
     """
@@ -73,6 +84,10 @@ ram_total_description_label = ttk.Label(frame, text="RAM usage:").grid(column=0,
 ram_used_and_total_label = ttk.Label(frame, text="")
 ram_used_and_total_label.grid(column=0, row=3)
 
+#Disk relaterte labels of buttons
+disk_total_description_label = ttk.Label(frame, text="Disk usage:").grid(column=1, row=2)
+disk_used_and_total_label = ttk.Label(frame, text="")
+disk_used_and_total_label.grid(column=1, row=3)
 
 
 #caller update label funksjonen
@@ -80,6 +95,7 @@ update_label(cpu_label, cpu_usage)
 update_label(batterypercent_label, battery_sensor, 0)
 update_label(batterysecsleft_label, battery_sensor, 1)
 update_label(ram_used_and_total_label, ram_usage, -1)
+update_label(disk_used_and_total_label, disk_usage, -1)
 
 
 root.mainloop()
